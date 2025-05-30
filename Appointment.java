8be5615e739d7c963f25cf7829ece743f785d472
RabiaAz <rabiaazizova680@gmail.com>
2025-05-30 16:26:00 +0800

    Update Appointment.java

--- Appointment.java
+++ Appointment.java
@@ -9,37 +9,64 @@ package com.mycompany.groupproject;
  * @author user
  */
 public class Appointment {
-    private String apptDate;
-    private String apptTime;
-    private String room;
-    private Doctor doctor;
     private Patient patient;
-    
-    public Appointment(String apptDate, String apptTime, String room) {
-        this.apptDate = apptDate;
-        this.apptTime = apptTime;
-        this.room = room;
-    }
+    private Doctor doctor;
+    private String date;
+    private String time;
+    private String room;
+    private boolean canceled;
 
-    // Getters and Setters
-    public void setDoctor(Doctor doctor) {
-        this.doctor = doctor;
-    }
-
-    public void setPatient(Patient patient) {
+    public Appointment(Patient patient, Doctor doctor, String date, String time, String room) {
         this.patient = patient;
+        this.doctor = doctor;
+        this.date = date;
+        this.time = time;
+        this.room = room;
+        this.canceled = false;
     }
 
-    public String getApptDate() {
-        return apptDate;
+    public Patient getPatient() {
+        return patient;
     }
 
-    public String getApptTime() {
-        return apptTime;
+    public Doctor getDoctor() {
+        return doctor;
     }
-    
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public boolean isCanceled() {
+        return canceled;
+    }
+
     public void cancel() {
-        // Logic to cancel the appointment
-        System.out.println("Appointment on " + apptDate + " at " + apptTime + " has been canceled.");
+        if (!canceled) {
+            canceled = true;
+            System.out.println("Appointment for " + patient.getName() + " with " + doctor.getName() + " on " + date + " at " + time + " has been canceled.");
+        } else {
+            System.out.println("Appointment already canceled.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Appointment{" +
+                "patient=" + patient.getName() +
+                ", doctor=" + doctor.getName() +
+                ", date='" + date + '\'' +
+                ", time='" + time + '\'' +
+                ", room='" + room + '\'' +
+                ", canceled=" + canceled +
+                '}';
     }
 }
