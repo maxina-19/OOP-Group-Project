51fb6dad84c5ac446f505d341ec8380667a5f120
RabiaAz <rabiaazizova680@gmail.com>
2025-05-30 16:28:58 +0800

    Update Doctor.java

--- Doctor.java
+++ Doctor.java
@@ -9,70 +9,31 @@ package com.mycompany.groupproject;
  * @author user
  */
 public class Doctor {
-    private String name;
+  public class Doctor extends Person {
     private String specialty;
-    private String availableTimings;
-    private String contactInfo;
+    private String workingHours;
 
-    public Doctor(String name, String specialty, String availableTimings, String contactInfo) {
-        this.name = name;
+    public Doctor(String name, String specialty, String workingHours, String email) {
+        super(name, email);
         this.specialty = specialty;
-        this.availableTimings = availableTimings;
-        this.contactInfo = contactInfo;
-    }
-
-    // Consult a patient and update the medical record
-    public void consult(Patient patient, MedicalRecord record) {
-        System.out.println("Consulting patient " + patient.getName() + "...");
-        record.setDiagnosis("Diagnosis for " + patient.getName());
-        System.out.println("Diagnosis: " + record.getDiagnosis());
-    }
-
-    // Prescribe treatment and link it to the medical record
-    public void prescribe(Patient patient, MedicalRecord record, String treatment) {
-        record.setTreatment(treatment);
-        Prescription prescription = new Prescription(1, record.getRecordID(), "Take the medicine twice a day.", this);
-        patient.receivePrescription(prescription);
-        System.out.println("Prescribed treatment: " + treatment + " to " + patient.getName());
-    }
-
-    // Create an appointment for a patient
-    public Appointment createAppointment(String apptDate, String apptTime, String room) {
-        Appointment appointment = new Appointment(apptDate, apptTime, room);
-        appointment.setDoctor(this);
-        System.out.println("Created appointment for " + apptDate + " at " + apptTime);
-        return appointment;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+        this.workingHours = workingHours;
     }
 
     public String getSpecialty() {
         return specialty;
     }
 
+    public String getWorkingHours() {
+        return workingHours;
+    }
+
     public void setSpecialty(String specialty) {
         this.specialty = specialty;
     }
 
-    public String getAvailableTimings() {
-        return availableTimings;
+    public void setWorkingHours(String workingHours) {
+        this.workingHours = workingHours;
     }
-
-    public void setAvailableTimings(String availableTimings) {
-        this.availableTimings = availableTimings;
-    }
-
-    public String getContactInfo() {
-        return contactInfo;
-    }
-
-    public void setContactInfo(String contactInfo) {
-        this.contactInfo = contactInfo;
+}
     }
 }
