0d82cc6afaf91207604883adb7c28b45d2e7dc24
mina <134504064+maxina-19@users.noreply.github.com>
2025-05-30 21:50:38 +0800

    Update Patient.java

--- Patient.java
+++ Patient.java
@@ -1,71 +1,107 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.groupproject;
 
-/**
- *
- * @author user
- */
 import java.util.ArrayList;
 
-public class Patient extends Person {
+public abstract class Patient {
+    private String name;
     private int age;
-    private String occupation;
+    private String contactInfo;
     private String medicalHistory;
-    private String patientID;
+    private String id;
     private MedicalRecord medicalRecord;
+    private ArrayList<Appointment> appointments;
+    private ArrayList<Prescription> prescriptions;
 
-    public Patient(String name, int age, String email, String occupation, String medicalHistory, String patientID) {
-        super(name, email);
+    public Patient(String name, int age, String contactInfo, String medicalHistory, String id) {
+        this.name = name;
         this.age = age;
-        this.occupation = occupation;
+        this.contactInfo = contactInfo;
         this.medicalHistory = medicalHistory;
-        this.patientID = patientID;
+        this.id = id;
+        this.appointments = new ArrayList<>();
+        this.prescriptions = new ArrayList<>();
+    }
+
+    // Register the patient
+    public void register() {
+        System.out.println("Patient " + name + " registered.");
+    }
+
+    // Visit a doctor (consultation and treatment)
+    public void visitDoctor(Doctor doctor, MedicalRecord record) {
+        doctor.consult(this, record);
+        doctor.prescribe(this, record, "Treatment for condition X");
+    }
+
+    // Schedule an appointment
+    public void scheduleAppointment(Appointment appointment) {
+        appointments.add(appointment);
+        appointment.setPatient(this);
+        System.out.println("Appointment scheduled for " + this.name + " on " + appointment.getApptDate()
+                + " at " + appointment.getApptTime());
+    }
+
+    // Receive a prescription
+    public void receivePrescription(Prescription prescription) {
+        prescriptions.add(prescription);
+        System.out.println("Prescription issued to " + this.name + ": " + prescription.getInstructions());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public int getAge() {
         return age;
     }
 
-    public String getOccupation() {
-        return occupation;
-    }
-
-    public String getMedicalHistory() {
-        return medicalHistory;
-    }
-
-    public String getPatientID() {
-        return patientID;
-    }
-
-    public MedicalRecord getMedicalRecord() {
-        return medicalRecord;
-    }
-
     public void setAge(int age) {
         this.age = age;
     }
 
-    public void setOccupation(String occupation) {
-        this.occupation = occupation;
+    public String getContactInfo() {
+        return contactInfo;
+    }
+
+    public void setContactInfo(String contactInfo) {
+        this.contactInfo = contactInfo;
+    }
+
+    public abstract String getPatientType();
+
+    public String getMedicalHistory() {
+        return medicalHistory;
     }
 
     public void setMedicalHistory(String medicalHistory) {
         this.medicalHistory = medicalHistory;
     }
 
-    public void setPatientID(String patientID) {
-        this.patientID = patientID;
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public MedicalRecord getMedicalRecord() {
+        return medicalRecord;
     }
 
     public void setMedicalRecord(MedicalRecord medicalRecord) {
         this.medicalRecord = medicalRecord;
     }
 
-    public void visitDoctor(Doctor doctor, MedicalRecord record) {
-        System.out.println(getName() + " visited " + doctor.getName());
+    public ArrayList<Appointment> getAppointments() {
+        return appointments;
+    }
+
+    public ArrayList<Prescription> getPrescriptions() {
+        return prescriptions;
     }
 }
