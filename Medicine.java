6d25b06633c686e37f4aeed1bb49e43695e493d3
RabiaAz <rabiaazizova680@gmail.com>
2025-05-30 16:31:56 +0800

    Update Medicine.java

--- Medicine.java
+++ Medicine.java
@@ -11,42 +11,44 @@ package com.mycompany.groupproject;
 public class Medicine {
     private String name;
     private String manufacturer;
-    private String dosage; // e.g. "500mg", "10ml"
-    private int quantityInStock;
-    private double pricePerUnit;
+    private String dosage;
+    private int quantity;
+    private double price;
 
-    public Medicine(String name, String manufacturer, String dosage, int quantityInStock, double pricePerUnit) {
+    public Medicine(String name, String manufacturer, String dosage, int quantity, double price) {
         this.name = name;
         this.manufacturer = manufacturer;
         this.dosage = dosage;
-        this.quantityInStock = quantityInStock;
-        this.pricePerUnit = pricePerUnit;
+        this.quantity = quantity;
+        this.price = price;
     }
 
-    public void dispense(int quantity) {
-        if (quantity <= quantityInStock) {
-            quantityInStock -= quantity;
-            System.out.println(quantity + " units of " + name + " dispensed.");
+    public void dispense(int amount) {
+        if (amount <= quantity) {
+            quantity -= amount;
+            System.out.println(amount + " units of " + name + " dispensed. Remaining: " + quantity);
         } else {
-            System.out.println("Not enough stock for " + name);
+            System.out.println("Not enough " + name + " in stock.");
         }
     }
 
-    public void restock(int quantity) {
-        quantityInStock += quantity;
-        System.out.println(name + " restocked. New stock: " + quantityInStock);
+    public String getName() {
+        return name;
     }
 
-    // Getters and Setters
-    public String getName() { return name; }
-    public String getManufacturer() { return manufacturer; }
-    public String getDosage() { return dosage; }
-    public int getQuantityInStock() { return quantityInStock; }
-    public double getPricePerUnit() { return pricePerUnit; }
+    public String getManufacturer() {
+        return manufacturer;
+    }
 
-    public void setName(String name) { this.name = name; }
-    public void setManufacturer(String manufacturer) { this.manufacturer = manufacturer; }
-    public void setDosage(String dosage) { this.dosage = dosage; }
-    public void setQuantityInStock(int quantityInStock) { this.quantityInStock = quantityInStock; }
-    public void setPricePerUnit(double pricePerUnit) { this.pricePerUnit = pricePerUnit; }
+    public String getDosage() {
+        return dosage;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }
