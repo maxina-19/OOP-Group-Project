a0028294ee0cf22f9bbd9a1227bc453f5ad90f30
mina <134504064+maxina-19@users.noreply.github.com>
2025-05-30 21:46:23 +0800

    Update Appointment.java

--- Appointment.java
+++ Appointment.java
@@ -1,72 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.groupproject;
-
-/**
- *
- * @author user
- */
 public class Appointment {
-    private Patient patient;
-    private Doctor doctor;
-    private String date;
-    private String time;
+    private String apptDate;
+    private String apptTime;
     private String room;
-    private boolean canceled;
-
-    public Appointment(Patient patient, Doctor doctor, String date, String time, String room) {
-        this.patient = patient;
-        this.doctor = doctor;
-        this.date = date;
-        this.time = time;
+    private Doctor doctor;
+    private Patient patient;
+    
+    public Appointment(String apptDate, String apptTime, String room) {
+        this.apptDate = apptDate;
+        this.apptTime = apptTime;
         this.room = room;
-        this.canceled = false;
     }
 
-    public Patient getPatient() {
-        return patient;
+    // Getters and Setters
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
     }
 
-    public Doctor getDoctor() {
-        return doctor;
+    public void setPatient(Patient patient) {
+        this.patient = patient;
     }
 
-    public String getDate() {
-        return date;
+    public String getApptDate() {
+        return apptDate;
     }
 
-    public String getTime() {
-        return time;
+    public String getApptTime() {
+        return apptTime;
     }
-
-    public String getRoom() {
-        return room;
-    }
-
-    public boolean isCanceled() {
-        return canceled;
-    }
-
+    
     public void cancel() {
-        if (!canceled) {
-            canceled = true;
-            System.out.println("Appointment for " + patient.getName() + " with " + doctor.getName() + " on " + date + " at " + time + " has been canceled.");
-        } else {
-            System.out.println("Appointment already canceled.");
-        }
-    }
-
-    @Override
-    public String toString() {
-        return "Appointment{" +
-                "patient=" + patient.getName() +
-                ", doctor=" + doctor.getName() +
-                ", date='" + date + '\'' +
-                ", time='" + time + '\'' +
-                ", room='" + room + '\'' +
-                ", canceled=" + canceled +
-                '}';
+        // Logic to cancel the appointment
+        System.out.println("Appointment on " + apptDate + " at " + apptTime + " has been canceled.");
     }
 }
